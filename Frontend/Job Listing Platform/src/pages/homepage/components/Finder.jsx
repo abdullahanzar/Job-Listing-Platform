4f1e09e238e5fcde6acfbff68530da2ad55f8fa0
abdullahanzar <31632960+abdullahanzar@users.noreply.github.com>
2023-06-15 11:47:52 +0530

    Navbar Complete

--- Frontend/Job Listing Platform/src/pages/homepage/components/Finder.jsx
+++ Frontend/Job Listing Platform/src/pages/homepage/components/Finder.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import './Finder.css'
+import React from "react";
+import "./Finder.css";
 
 export default function Finder() {
   return (
-    <div className='finder'>
-        <input type="text" name="Job" id="SearchJob" placeholder={`&#128269; `}/>
-    
+    <div className="finder">
+      <input type="text" name="Job" id="SearchJob" placeholder={`&#128269; `} />
     </div>
-  )
+  );
 }
